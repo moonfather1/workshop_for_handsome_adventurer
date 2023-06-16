90b1bcfba0467021e4004bb60ea13099c46426ac
MoonFather <moonfather@homemail.com>
2023-06-16 14:58:26 +0100

    version

--- src/main/java/moonfather/workshop_for_handsome_adventurer/ModWorkshop.java
+++ src/main/java/moonfather/workshop_for_handsome_adventurer/ModWorkshop.java
@@ -22,29 +22,7 @@ import org.slf4j.Logger;
 @Mod(Constants.MODID)
 public class ModWorkshop
 {
-    //bug !- xp nuggets in recipes
-    //bug !- workbench and bare hand
     //MFO FD board, 1.18 create
-    //--before--pub--
-    //todo: printLn, CR in toml, ver
-    //--known-- issues
-    //integration: Carry On blacklist is broken in 1.17.0.8 and a few older versions. do not pick up big tables and racks, if you do, type /kill. (actually works on dedicated servers but broken on integrated.)  Carry On developer will fix the blacklist eventually.
-    //integration: WTHIT is entirely unsupported, Jade and TOP are fine. it's not my fault, i wanted to add support but i can not. blame wthit author while you switch to an alternative.
-    //vanilla behavior: when you have two-block rack and you're looking so that the top half isn't visible, items disappear. i think it's not that much of a problem.
-    //integration: crescent hammer (thermal) won't go onto the rack.  it's fine.
-	//integration: items in tetra belt storage are shown in different order than what you might expect. tetra displays rows from bottom to top (bottom-most holds items that come first in storage); while we could make a dedicated container which re-maps slot numbers, there is still an issue of us having 9 slots per row and original gui having 8. if people really want, we can fix it.
-	//integration: items in toolboxes from create mod are shown weird. not our fault, create internally uses 4 slots of 64 items and shows one slot with a max of 256. we could make a dedicated container for that but it won't be easy. maybe for a large number of downloads.
-    //--later--
-    //after 1.0: render items on top of tables
-    //after 1.0: todo list similar to one in BiblioCraft     (edit: apparently there will be one in create mod soon so i might cross this off)
-    //after 1.0: bookshelf like the one in BiblioCraft; no gui; (edit: apparently exactly that is coming in 1.20)
-    //after 1.1: double the number of supported adjacent chests
-	//after 1.1: chest markers (similar to item frames); maybe? (no gui, pull items from chest)
-    //maybe after 1.1: storage drawer support. maybe. not sure how. in any case, only the closest blocks.
-    //maybe after 1.1: sword rack? likely not. (decorative, single sword, horizontal, no gui)
-    //maybe after 1.2: tetra tables in wood variants
-    //will not: weapon rack (vertical weapons),  sword and shield on wall... j
-
 
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
