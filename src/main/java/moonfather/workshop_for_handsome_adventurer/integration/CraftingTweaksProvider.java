c5bc551a94ea8c3db494e1065d369adb69d333d1
MoonFather <moonfather@homemail.com>
2023-10-11 13:34:00 +0100

    crafting tweaks support.

---
+++ src/main/java/moonfather/workshop_for_handsome_adventurer/integration/CraftingTweaksProvider.java
@@ -0,0 +1,42 @@
+package moonfather.workshop_for_handsome_adventurer.integration;
+
+import moonfather.workshop_for_handsome_adventurer.Constants;
+import moonfather.workshop_for_handsome_adventurer.block_entities.DualTableMenu;
+import moonfather.workshop_for_handsome_adventurer.block_entities.SimpleTableMenu;
+import net.blay09.mods.craftingtweaks.api.*;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+
+public class CraftingTweaksProvider implements CraftingGridProvider
+{
+    public CraftingTweaksProvider()
+    {
+        CraftingTweaksAPI.registerCraftingGridProvider(this);
+    }
+
+    @Override
+    public String getModId()
+    {
+        return Constants.MODID;
+    }
+
+    @Override
+    public boolean handles(AbstractContainerMenu menu)
+    {
+        return menu instanceof SimpleTableMenu;
+    }
+
+    @Override
+    public void buildCraftingGrids(CraftingGridBuilder builder, AbstractContainerMenu menu)
+    {
+        if (menu instanceof DualTableMenu) {
+            builder.addGrid("primary", DualTableMenu.CRAFT_SLOT_START, 3*3)
+                   .setButtonAlignment(ButtonAlignment.LEFT);
+            builder.addGrid("secondary", DualTableMenu.CRAFT_SECONDARY_SLOT_START, 3*3)
+                   .setButtonAlignment(ButtonAlignment.LEFT);
+        }
+        else if (menu instanceof SimpleTableMenu) {
+            builder.addGrid("single", SimpleTableMenu.CRAFT_SLOT_START, 3*3)
+                   .setButtonAlignment(ButtonAlignment.LEFT);
+        }
+    }
+}
